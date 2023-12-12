55da89e649fc5cb5d1d605f000d06438b061846d
GanaF4ll <ganafall9498@gmail.com>
2023-12-12 15:41:50 +0100

    timerModel creation

---
+++ models/timerModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let timerSchema = new Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Number,
+    required: true,
+  },
+});
