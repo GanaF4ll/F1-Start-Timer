a710d6fbc1a5e8e316afffccdbc2666814d6f81b
Gana <ganafall9498@gmail.com>
2023-12-12 19:21:57 +0100

    app crash fix: added module.exports to timerRoute

--- routes/timerRoute.js
+++ routes/timerRoute.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const timerController = require("../controllers/timerController");
 
-router.get("/user_id/timers", timerController.getAllTimers);
-// url = http://localhost:3001/user_id/timers
-
-router.post("/user_id/create/timer", timerController.createATimer);
+router
+  //   .get("/user_id/timers", timerController.getAllTimers)
+  // url = http://localhost:3001/user_id/timers
+  .post("/user_id/create/timer", timerController.createATimer);
 // url = http://localhost:3001/user_id/create/timer
+http: module.exports = router;
