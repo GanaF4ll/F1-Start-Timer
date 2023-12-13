75eda8d5716dd2c1303c83c04f1166a98523b640
Gana <ganafall9498@gmail.com>
2023-12-13 11:12:02 +0100

    updated userDelete

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,12 @@ exports.userRegister = async (req, res) => {
   try {
     let newUser = new User(req.body);
     let user = await newUser.save();
-    res.status(201).json({ message: `Utilisateur crée: ${user.email}` });
+    res.status(201).json({ message: `Utilisateur créé : ${user.email}` });
   } catch (error) {
-    console.log(error);
-    res.status(401).json({ message: "Requete invalide" });
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la création de l'utilisateur" });
   }
 };
 
@@ -17,45 +19,42 @@ exports.userLogin = async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
 
-    if (!user) {
-      res.status(500).json({ message: "utilisateur non trouvé" });
+    if (!user || user.password !== req.body.password) {
+      res.status(401).json({ message: "Email ou mot de passe incorrect" });
       return;
     }
 
-    if (user.email === req.body.email && user.password === req.body.password) {
-      const userData = {
-        id: user._id,
-        email: user.email,
-        role: "admin",
-      };
-      const token = await jwt.sign(userData, process.env.JWT_KEY, {
-        expiresIn: "10h",
-      });
-      res.status(200).json({ token });
-    } else {
-      res.status(401).json({ message: "Email ou mot de passe incorrect" });
-    }
+    const userData = {
+      id: user._id,
+      email: user.email,
+      role: "admin",
+    };
+    const token = jwt.sign(userData, process.env.JWT_KEY, {
+      expiresIn: "10h",
+    });
+
+    res.status(200).json({ token });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res
       .status(500)
-      .json({ message: "Une erreur s'est produite lors du traitement" });
+      .json({ message: "Une erreur s'est produite lors de la connexion" });
   }
 };
 
-exports.userDelete = async (res, req) => {
+exports.userDelete = async (req, res) => {
   try {
     const user = await User.findOneAndDelete({ email: req.body.email });
     if (user) {
-      res.status(201);
-      res.status(201).json({ message: `Utilisateur supprimé: ${user.email}` });
+      res.status(200).json({ message: `Utilisateur supprimé : ${user.email}` });
     } else {
-      res.status(500).json({ message: "L'utilisateur n'existe pas" });
-      return;
+      res.status(404).json({ message: "L'utilisateur n'existe pas" });
     }
   } catch (error) {
-    res.status(500);
-    console.log(error);
-    res.json({ message: "Une erreur s'est produite lors du traitement" });
+    console.error(error);
+    res.status(500).json({
+      message:
+        "Une erreur s'est produite lors de la suppression de l'utilisateur",
+    });
   }
 };
