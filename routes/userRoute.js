a5dd33b79d1a3b67bb790ec9c2f888ae8a2ec3cb
Gana <ganafall9498@gmail.com>
2023-12-13 19:42:50 +0100

    deleted some comments

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const userController = require("../controllers/userController");
-
-// router.post("/user/register", userController.userRegister);
-// // url = http://localhost:3001/user/register
-
-// router.post("/user/login", userController.userLogin);
-// // url = http://localhost:3001/user/login
-
-// router.delete("/user/delete/:email", userController.userDelete);
-// // url = http://localhost:3001/user/delete/:email
-
-// router.put("/user/update/:email", userController.userUpdate);
-// // url = http://localhost:3001/user/update/:email
-// http: module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
@@ -26,7 +9,6 @@ router.post("/user/register", userController.userRegister);
 router.post("/user/login", userController.userLogin);
 // url = http://localhost:3001/user/login
 
-// Ajout du middleware verifyToken avant les routes protégées
 router.delete("/user/delete/:email", verifyToken, userController.userDelete);
 // url = http://localhost:3001/user/delete/:email
 
