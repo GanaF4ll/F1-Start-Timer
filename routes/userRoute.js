58cb414da45deb554617dbdfcc7afcf09996a67e
Gana <ganafall9498@gmail.com>
2023-12-13 19:39:11 +0100

    rename folder "middleware" into "middlewares", test token verification

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,24 @@
+// const express = require("express");
+// const router = express.Router();
+// const userController = require("../controllers/userController");
+
+// router.post("/user/register", userController.userRegister);
+// // url = http://localhost:3001/user/register
+
+// router.post("/user/login", userController.userLogin);
+// // url = http://localhost:3001/user/login
+
+// router.delete("/user/delete/:email", userController.userDelete);
+// // url = http://localhost:3001/user/delete/:email
+
+// router.put("/user/update/:email", userController.userUpdate);
+// // url = http://localhost:3001/user/update/:email
+// http: module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { verifyToken } = require("../middlewares/jwtMiddleware");
 
 router.post("/user/register", userController.userRegister);
 // url = http://localhost:3001/user/register
@@ -8,9 +26,11 @@ router.post("/user/register", userController.userRegister);
 router.post("/user/login", userController.userLogin);
 // url = http://localhost:3001/user/login
 
-router.delete("/user/delete/:email", userController.userDelete);
+// Ajout du middleware verifyToken avant les routes protégées
+router.delete("/user/delete/:email", verifyToken, userController.userDelete);
 // url = http://localhost:3001/user/delete/:email
 
-router.put("/user/update/:email", userController.userUpdate);
+router.put("/user/update/:email", verifyToken, userController.userUpdate);
 // url = http://localhost:3001/user/update/:email
-http: module.exports = router;
+
+module.exports = router;
