7baa16f9bd7dc011243f44b7c3ff32ba799d5b19
Gana <ganafall9498@gmail.com>
2023-12-14 10:43:02 +0100

    deleted front

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleLightClass(event) {
-  if (event.target.classList.contains("light")) {
-    event.target.classList.toggle("light-strip.on");
-  }
-}
-
-// Ajout de l'écouteur d'événements au clic sur le document
-document.addEventListener("click", toggleLightClass);
